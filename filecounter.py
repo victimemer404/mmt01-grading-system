c92adad7d3a88fddad24ce6239efdf7e3fe6c27b
victimemer404 <68136321+victimemer404@users.noreply.github.com>
2020-10-03 22:22:32 +0800

    Add files via upload

--- filecounter.py
+++ filecounter.py
@@ -5,21 +5,25 @@ for files in FILES:
     with open(files, "r") as activeFile:
         data = activeFile.readlines()
 
-    safe = 0
-    unsafe = 0
+    one = 0
+    five = 0
+    hundred = 0
 
     for line in data:
-        if float(line) < 0.05:
-            safe += 1
+        if float(line) < 0.01:
+            one += 1
+        elif 0.01 <= float(line) < 0.05:
+            five += 1
         else:
-            unsafe += 1
+            hundred += 1
 
     if fileIndexCounter == 0:
         print("5 SUBJECTS")
     else:
         print("10 SUBJECTS")
 
-    print("Safe {} values".format(safe))
-    print("Unsafe {} values".format(unsafe))
+    print("99% {} values".format(one))
+    print("95% {} values".format(five))
+    print("Unsafe {} values".format(hundred))
 
     fileIndexCounter += 1
